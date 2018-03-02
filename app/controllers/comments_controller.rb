0b32c6c3f0d8006b69fb593a8d1be13ede8f65cc
krivtsov <krivtsov.su@gmail.com>
2018-03-03 00:01:36 +0400

    auto refactoring rubocop

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,7 +16,7 @@ class CommentsController < ApplicationController
 
   private
 
-    def comment_params
-      params.require(:comment).permit(:commenter, :body)
-    end
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
 end
