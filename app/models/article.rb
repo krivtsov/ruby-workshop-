afc321fbce054eff586dbc0ec620a69dd7850c01
krivtsov <krivtsov.su@gmail.com>
2018-03-03 18:07:21 +0400

    add validate state

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,6 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
-  validates :title, presence: true,
-                    length: { minimum: 5 }
-  validates :author_name, presence: true,
-                    length: { minimum: 4 }
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :author_name, presence: true, length: { minimum: 4 }
+  validates :state, inclusion: { in: %w[draft published] }
 end
