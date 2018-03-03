0665b312ed12174ccc583bead4ff368be53d4654
krivtsov <krivtsov.su@gmail.com>
2018-03-03 11:05:58 +0400

    add field state

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180302201636) do
+ActiveRecord::Schema.define(version: 20180303061553) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180302201636) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "author_name"
+    t.string "state"
   end
 
   create_table "comments", force: :cascade do |t|
